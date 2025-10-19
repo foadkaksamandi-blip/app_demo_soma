a1cecfe9467fdc9860092d7c932b802ba03e04bb
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-20 02:51:30 +0330

    Create QRGen.kt

---
+++ consumer-app/src/main/java/com/soma/consumer/qr/QRGen.kt
@@ -0,0 +1,25 @@
+package com.soma.consumer.qr
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
+
+object QRGen {
+    fun make(data: String, size: Int = 512): Bitmap {
+        val hints = hashMapOf<EncodeHintType, Any>(
+            EncodeHintType.CHARACTER_SET to "UTF-8",
+            EncodeHintType.ERROR_CORRECTION to ErrorCorrectionLevel.M
+        )
+        val bitMatrix = QRCodeWriter().encode(data, BarcodeFormat.QR_CODE, size, size, hints)
+        val bmp = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
+        for (x in 0 until size) {
+            for (y in 0 until size) {
+                bmp.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+            }
+        }
+        return bmp
+    }
+}
