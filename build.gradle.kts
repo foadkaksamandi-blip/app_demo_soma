efb16c3a63d80fccb823a130dca4cbca689db799
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-20 03:18:58 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    // هیچ پلاگینی در ریشه لازم نیست
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
-allprojects {
-    // می‌توان تنظیمات مشترک را اینجا گذاشت
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
