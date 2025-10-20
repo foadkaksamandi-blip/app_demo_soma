3ae8f7345d10d6b1f83a9373e13055a3a1c03a71
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-20 21:50:49 +0330

    Update build.gradle.kts

--- consumer-app/build.gradle.kts
+++ consumer-app/build.gradle.kts
@@ -33,9 +33,10 @@ android {
     kotlinOptions { jvmTarget = "17" }
 
     buildFeatures {
-        viewBinding = true
         compose = true
+        viewBinding = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = property("compose.compiler").toString()
     }
@@ -52,16 +53,18 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.2")
 
-    // Compose
+    // Compose UI
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // Material3
     implementation("androidx.compose.material3:material3:1.2.1")
 
-    // Material XML
+    // Material XML Theme
     implementation("com.google.android.material:material:1.12.0")
 
-    // QR
+    // ZXing (QR)
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("com.google.zxing:core:3.5.1")
 
